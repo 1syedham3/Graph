667fefbe4a062bed0ab291c0c842b03023b63b95
1syedham3 <126813340+1syedham3@users.noreply.github.com>
2024-03-03 15:58:21 +0000

    fixed the input fields and added circles

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ quadratic = {
   c: 0
 }
 
+Circle = {
+  h: 0,
+  k: 0,
+  r: 5
+}
+
 function setup() {
   createCanvas(575, 575);
   zoom = createSlider(50, 200, 100);
@@ -31,11 +37,16 @@ function setup() {
       b: createInput("0", "number"),
       c: createInput("0", "number")
     },
+    circle: {
+      h: createInput("0", "number"),
+      k: createInput("0", "number"),
+      r: createInput("5", "number"),
+    },
     solver: createSelect()
   }
   inputs.solver.option('Roots of a parabola');
   inputs.solver.option('Parabola intercepting with a line');
-  inputs.solver.option('circle intercepting with a line');
+  inputs.solver.option('Circle intercepting with a line');
   inputs.solver.selected('Parabola intercepting with a line');
   inputs.solver.position(350, 515)
 }
@@ -55,6 +66,10 @@ function draw() {
   quadratic.b = inputs.quadratic.b.value();
   quadratic.c = inputs.quadratic.c.value();
 
+  Circle.h = inputs.circle.h.value();
+  Circle.k = inputs.circle.k.value();
+  Circle.r = inputs.circle.r.value();
+
   let increment;
   if (zoom.value() < 75) { increment = 2; } else { increment = 1; }
   stroke(50);
@@ -67,20 +82,32 @@ function draw() {
   line(0, -5000, 0, 5000);
   line(-5000, 0, 5000, 0);
 
-  // line
   strokeWeight((100 / zoom.value() * 5));
-  stroke(75, 200, 75);
-  line(-5000, -(liner.m * -5000 + liner.b * 25), 5000, -(liner.m * 5000 + liner.b * 25));
+
+  // line
+  if (inputs.solver.selected() != 'Roots of a parabola') {
+    stroke(75, 200, 75);
+    line(-5000, -(liner.m * -5000 + liner.b * 25), 5000, -(liner.m * 5000 + liner.b * 25));
+  }
 
   // quadratic
-  noFill();
-  stroke(0, 0, 255);
-  beginShape();
-  for (let x = (100 / zoom.value() * -5000); x < (100 / zoom.value() * 5000); x += 1.5) {
+  if (inputs.solver.selected() != 'Circle intercepting with a line') {
+    noFill();
+    stroke(0, 0, 255);
+    beginShape();
+    for (let x = (100 / zoom.value() * -5000); x < (100 / zoom.value() * 5000); x += 1.5) {
 
-    vertex(x * 5, -(quadratic.a * x * x + quadratic.b * x + quadratic.c * 25));
+      vertex(x * 5, -(quadratic.a * x * x + quadratic.b * x + quadratic.c * 25));
+    }
+    endShape();
   }
-  endShape();
+
+  // circle
+  if (inputs.solver.selected() == 'Circle intercepting with a line') {
+    noFill();
+    stroke(255, 0, 0);
+    circle(-Circle.h * 25, -Circle.k * 25, Circle.r * 50);
+  }
 
   // scale
   textStyle(NORMAL);
@@ -105,13 +132,42 @@ function draw() {
   textSize(15);
   textStyle(BOLD);
   textAlign(LEFT);
+
+  //input fields
   if (inputs.solver.selected() != 'Roots of a parabola') {
-    text("f(x) =              x + ", 60, 540);
+    text("f(x) =              x  + ", 60, 540);
     inputs.liner.m.position(110, 537);
-    inputs.liner.b.position(190, 537);
+    inputs.liner.b.position(195, 537);
   } else {
     inputs.liner.m.position(0, -100);
     inputs.liner.b.position(0, -100);
+  }
+
+  if (inputs.solver.selected() == 'Circle intercepting with a line') {
+    text("c(x) = ( x -              )² +  (y -              )² =             ²", 60, 563);
+    inputs.circle.h.position(145, 560);
+    inputs.circle.k.position(255, 560);
+    inputs.circle.r.position(337, 560);
+  } else {
+    inputs.circle.h.position(0, -100);
+    inputs.circle.k.position(0, -100);
+    inputs.circle.r.position(0, -100);
+  }
+
+  if (inputs.solver.selected() == 'Parabola intercepting with a line') {
+    text("g(x) =              x² +              x +", 57, 563);
+    inputs.quadratic.a.position(110, 560);
+    inputs.quadratic.b.position(195, 560);
+    inputs.quadratic.c.position(275, 560);
+  } else if (inputs.solver.selected() == 'Roots of a parabola') {
+    text("g(x) =              x² +              x +", 57, 540);
+    inputs.quadratic.a.position(110, 537);
+    inputs.quadratic.b.position(195, 537);
+    inputs.quadratic.c.position(275, 537);
+  } else {
+    inputs.quadratic.a.position(0, -100);
+    inputs.quadratic.b.position(0, -100);
+    inputs.quadratic.c.position(0, -100);
   }
 
   text("Zoom: " + zoom.value() + "%", 10, 517);
